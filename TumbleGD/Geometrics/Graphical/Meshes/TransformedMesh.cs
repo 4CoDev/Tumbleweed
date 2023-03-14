2d933d5119000ae94e67d50ae74254859ac67df6
4CoDev <4Co_dev@mail.ru>
2023-03-14 14:29:38 +0500

    Added transformed mesh class

---
+++ TumbleGD/Geometrics/Graphical/Meshes/TransformedMesh.cs
@@ -0,0 +1,24 @@
+using Godot;
+using TumbleGD.Geometrics.Graphical.Surfaces;
+using Tumbleweed.Enumerables;
+using Tumbleweed.Scalars;
+
+namespace TumbleGD.Geometrics.Graphical.Meshes;
+
+public sealed class TransformedMesh : MeshEnvelope
+{
+	public TransformedMesh
+	(
+		IMesh mesh,
+		IScalar<Transform3D> transform
+	) : base
+	(
+		new MeshWithSurfaces(
+			new TransformedSurfaces(
+				new EnumerableOfDelegate<ISurface>(
+					() => mesh.Surfaces),
+				transform))
+	)
+	{
+	}
+}
