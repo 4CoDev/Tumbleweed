ba1b7154e67ef9e6a2be9c217f5c34a75f5959c6
4CoDev <4Co_dev@mail.ru>
2023-03-26 21:52:51 +0500

    Added clones on path

---
+++ TumbleGD/Nodes/FromFiles/OfGodot/CloneOnPath.cs
@@ -0,0 +1,21 @@
+using Godot;
+using Tumbleweed.Scalars;
+
+namespace TumbleGD.Nodes.FromFiles.OfGodot;
+
+public sealed class CloneOnPath : ScalarEnvelope<Node> 
+{
+	public CloneOnPath(String path) : this
+	(
+		new ScalarOfValue<String>(path)
+	)
+	{
+	}
+	
+	public CloneOnPath(IScalar<String> path) : base
+	(
+		new CloneOnPath<Node>(path)
+	)
+	{
+	}
+}
---
+++ TumbleGD/Nodes/FromFiles/OfGodot/CloneOnPath`1.cs
@@ -0,0 +1,23 @@
+using Godot;
+using TumbleGD.Resources.FromFiles.ToGodot;
+using Tumbleweed.Scalars;
+
+namespace TumbleGD.Nodes.FromFiles.OfGodot;
+
+public sealed class CloneOnPath<T> : ScalarEnvelope<T> where T : Node
+{
+	public CloneOnPath(String path) : this
+	(
+		new ScalarOfValue<String>(path)
+	)
+	{
+	}
+	
+	public CloneOnPath(IScalar<String> path) : base
+	(
+		new LazyValue<T>(
+			new NodeOnPath<T>(path))
+	)
+	{
+	}
+}
