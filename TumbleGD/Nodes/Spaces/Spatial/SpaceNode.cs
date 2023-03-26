60a9e91b32559091e2ad48e83e82690ab39c4f8d
4CoDev <4Co_dev@mail.ru>
2023-03-26 16:27:33 +0500

    Transform space to 3d node

--- TumbleGD/Nodes/Spaces/Spatial/SpaceNode.cs
+++ TumbleGD/Nodes/Spaces/Spatial/SpaceNode.cs
@@ -1,22 +1,23 @@
 using Godot;
+using TumbleGD.Nodes.Children;
 using Tumbleweed.Scalars;
 
 namespace TumbleGD.Nodes.Spaces.Spatial;
 
-public sealed class SpaceNode : ScalarEnvelope<Node>
+public sealed class SpaceNode : ScalarEnvelope<Node3D>
 {
 	public SpaceNode() : base
 	(
-		new ValueOfFunction<Node>(Function)
+		new ValueOfFunction<Node3D>(Function)
 	)
 	{
 	}
 
-	private static Node Function()
+	private static Node3D Function()
 	{
-		Node space = new Node { Name = "Space" };
+		Node3D space = new Node3D { Name = "Space" };
 		MeshInstance3D mesh = new MeshInstance3D { Name = "Mesh" };
-		space.AddChild(mesh);
+		new ChildrenOfNode(space).Add(mesh);
 		return space;
 	}
 }
