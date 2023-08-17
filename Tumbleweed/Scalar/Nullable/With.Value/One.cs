87700205a5ccbb27aae0cf448ecdfdcde17a652f
4CoDev <4Co_dev@mail.ru>
2023-08-17 13:08:14 +0500

    Add nullable with value class

---
+++ Tumbleweed/Scalar/Nullable/With.Value/One.cs
@@ -0,0 +1,23 @@
+using Nullable = Tumbleweed.Scalar.Nullable;
+using Immutable = Tumbleweed.Scalar.Immutable;
+using Boolean = Tumbleweed.Boolean;
+
+namespace Tumbleweed.Scalar.Nullable.With.Value;
+
+public sealed class One<T> : Nullable::Envelope<T>
+{
+	public One(T value) : this
+	(
+		new Immutable::With.Value<T>(value)
+	)
+	{
+	}
+	
+	public One(Immutable::Any<T> value) : base
+	(
+		new Nullable::With.Members<T>(
+			value,
+			new Boolean::Literal.True()))
+	{
+	}
+}
