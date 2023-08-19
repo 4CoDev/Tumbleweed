67b0844ac71855c7d23624e5b4ee7c93184b0659
4CoDev <4Co_dev@mail.ru>
2023-08-19 12:38:50 +0500

    Add unmeasured batch with members class

---
+++ DotGod/Node/Batch/Dimension.Spatial/Measurement/Unmeasured/With.Members/One.cs
@@ -0,0 +1,24 @@
+using Batch = DotGod.Node.Batch.Dimension.Spatial.Measurement.Unmeasured;
+using Array = Tumbleweed.Array;
+using Nullable = Tumbleweed.Scalar.Nullable;
+using SCG = System.Collections.Generic;
+using Entity = DotGod.Node.Batch.Dimension.Spatial.Entity;
+
+namespace DotGod.Node.Batch.Dimension.Spatial.Measurement.Unmeasured.With.Members;
+
+public sealed class One : Batch::Any
+{
+	public One
+	(
+		Array::Any<Nullable::Any<Batch::Any>> subbatches,
+		SCG::ICollection<Entity::Any> entities
+	)
+	{
+		Subbatches = subbatches;
+		Entities = entities;
+	}
+
+	public Array::Any<Nullable::Any<Batch::Any>> Subbatches { get; }
+
+	public SCG::ICollection<Entity::Any> Entities { get;}
+}
